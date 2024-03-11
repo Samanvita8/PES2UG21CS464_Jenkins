c2d79016a89a0011ce5119df88633ea7dccc5227
Samanvita8 <samanvitagprasad1404@gmail.com>
2024-03-11 11:57:26 +0530

    Describe this commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "My name is Samanvita G Prasad\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
