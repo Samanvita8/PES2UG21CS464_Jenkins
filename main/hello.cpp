121507923f5939e3bbc7e801dc60fc94d4e37cc1
Samanvita G Prasad <93179882+Samanvita8@users.noreply.github.com>
2024-03-11 22:08:57 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "My name is Samanvita G Prasad\n";
-  cout << "I have successfully built and run\n";
+  cout << "How are you doing?\n";
   return 0;
 }
